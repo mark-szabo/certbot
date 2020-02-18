9fba7ea759ec2a33d521c2fe6a9a72df59d407e7
Mark Szabo <a-marks@microsoft.com>
2020-02-18 19:43:54 +0100

    DI & token provider

---
+++ Certbot/AuthenticationTokenProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Rest;
+
+namespace Certbot
+{
+    internal class AuthenticationTokenProvider : ITokenProvider
+    {
+        private readonly AzureServiceTokenProvider _tokenProvider = new AzureServiceTokenProvider();
+
+        public async Task<AuthenticationHeaderValue> GetAuthenticationHeaderAsync(CancellationToken cancellationToken)
+        {
+            var accessToken = await _tokenProvider.GetAccessTokenAsync("https://management.azure.com/");
+
+            return new AuthenticationHeaderValue("Bearer", accessToken);
+        }
+    }
+}
---
+++ Certbot/Startup.cs
@@ -0,0 +1,24 @@
+﻿using DnsClient;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(Certbot.Startup))]
+
+namespace Certbot
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddHttpClient();
+
+            builder.Services.AddSingleton(new LookupClient { UseCache = false });
+
+            builder.Services.AddSingleton(provider =>
+                new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(new AzureServiceTokenProvider().KeyVaultTokenCallback)));
+
+        }
+    }
+}
